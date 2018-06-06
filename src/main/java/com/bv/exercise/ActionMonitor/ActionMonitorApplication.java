a08a2aa85081a7238bf5ec1754654b65e3ee6ad9
PTomi002 <tamas.paulin91@gmail.com>
2018-06-06 22:52:22 +0200

    initial commit

---
+++ src/main/java/com/bv/exercise/ActionMonitor/ActionMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.bv.exercise.ActionMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionMonitorApplication.class, args);
+	}
+}
