c377457ec447b745a3b054cef1210613833432a6
Tamas David Paulin <tamas.paulin@evosoft.com>
2018-06-08 11:15:35 +0200

    adding POST mapping and tests for TimeSeries function

--- src/main/java/com/bv/exercise/ActionMonitor/controller/TimeSeriesController.java
+++ src/main/java/com/bv/exercise/ActionMonitor/controller/TimeSeriesController.java
@@ -5,6 +5,8 @@ import com.bv.exercise.ActionMonitor.model.TimeSeries;
 import com.bv.exercise.ActionMonitor.repository.TimeSeriesRepository;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/timeseries")
@@ -23,26 +27,29 @@ public class TimeSeriesController {
     private final TimeSeriesRepository timeSeriesRepository;
 
     @PostMapping
-    public void insertTime(@RequestBody final TimeSeries time) {
-        //TODO: Insert into DB
+    @ResponseStatus(HttpStatus.CREATED)
+    public TimeSeries insertTime(@Validated @RequestBody final TimeSeries timeSeries) {
+        log.info("Inserting into database with: {}", timeSeries);
+        return timeSeriesRepository.save(timeSeries);
     }
 
     @PutMapping
     public void updateTime(@RequestBody final TimeSeries time) {
-        //TODO: Update an already existing time series with the request body
     }
 
     @GetMapping("/{id}")
     public TimeSeries getTime(@Validated @PathVariable final String id) {
+        log.info("Get time series data with: {}", id);
         final TimeSeries foundTimeSeries = timeSeriesRepository.findOne(id);
         if (Objects.isNull(foundTimeSeries)) {
+            log.warn("Time series data not found with: {}", id);
             throw new NoSuchTimeSeriesException("No time series data found with: " + id + "!");
         }
         return foundTimeSeries;
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTime(@PathVariable final String id) {
-
     }
 }
--- src/test/java/com/bv/exercise/ActionMonitor/controller/TimeSeriesControllerTest.java
+++ src/test/java/com/bv/exercise/ActionMonitor/controller/TimeSeriesControllerTest.java
@@ -1,6 +1,8 @@
 package com.bv.exercise.ActionMonitor.controller;
 
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,11 +10,14 @@ import com.bv.exercise.ActionMonitor.ActionMonitorApplication;
 import com.bv.exercise.ActionMonitor.model.TimeSeries;
 import com.bv.exercise.ActionMonitor.repository.TimeSeriesRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -51,6 +56,11 @@ public class TimeSeriesControllerTest {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
+    @After
+    public void tearDown() {
+        timeSeriesRepository.deleteAll();
+    }
+
     @Test
     public void tesGetNotExistingTimeSeriesData() throws Exception {
         mockMvc.perform(get(BASE_URL + NOT_EXISTING_ID))
@@ -62,5 +72,23 @@ public class TimeSeriesControllerTest {
         mockMvc.perform(get(BASE_URL + ID))
             .andExpect(status().isOk())
             .andExpect(content().string(objectMapper.writeValueAsString(TIME_SERIES)));
+    }
+
+    @Test
+    public void testInsertTimeSeriesData() throws Exception {
+        final TimeSeries timeSeries = createTimeSeries("abc-2", 20L);
+
+        mockMvc.perform(
+            post(BASE_URL).content(objectMapper.writeValueAsString(timeSeries)).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated());
+
+        assertThat("There should be a plus time series object!", timeSeriesRepository.count(), CoreMatchers.is(2L));
+    }
+
+    private TimeSeries createTimeSeries(final String id, final Long time) {
+        final TimeSeries timeSeries = new TimeSeries();
+        timeSeries.setId(id);
+        timeSeries.setTime(time);
+        return timeSeries;
     }
 }
