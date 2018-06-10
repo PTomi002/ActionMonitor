494cfe3cd5ef27678b592a1bfe81289da6ad7984
PTomi002 <tamas.paulin91@gmail.com>
2018-06-10 20:07:49 +0200

    run tests in memory H2

--- src/test/java/com/bv/exercise/ActionMonitor/controller/TimeSeriesControllerTest.java
+++ src/test/java/com/bv/exercise/ActionMonitor/controller/TimeSeriesControllerTest.java
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -29,6 +30,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ActionMonitorApplication.class)
+@TestPropertySource(properties = "spring.datasource.url=jdbc:h2:mem:dev")
 public class TimeSeriesControllerTest {
 
     private static final String BASE_URL = "/timeseries/";
