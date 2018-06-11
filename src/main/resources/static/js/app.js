9b4c41259a29d83e51f5c4539dd25787099f2fac
PTomi002 <tamas.paulin91@gmail.com>
2018-06-11 18:44:42 +0200

    add UI for websocket and STOMP

---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,45 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#db_logs").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/db_actions_stomp');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/db_actions', function (dbAction) {
+            showDbAction(JSON.parse(dbAction.body));
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function showDbAction(message) {
+    $("#db_logs").append("<tr><td>DB Action happened: " + message.executionType + " with id: " + message.id + " with time: " + message.time + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+});
